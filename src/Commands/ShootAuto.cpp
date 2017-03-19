bab1e5d7ec7993f05c7079959167814253571642
Benjamin Davis <greendartfrog@gmail.com>
2017-03-19 14:48:59 -0400

    Made ShootAuto more like other routines

--- src/Commands/ShootAuto.cpp
+++ src/Commands/ShootAuto.cpp
@@ -5,26 +5,29 @@
 #include "DriveCartesian.h"
 #include "Kick.h"
 
+#define SHOOTER_POWER 0.7
 
-#define SHOOTER_POWER .7
 
 ShootAuto::ShootAuto() {
+	AddParallel(new DriveStraight(1.3,0.5));
+	AddSequential(new SpinUp(1.3, -SHOOTER_POWER));
+
 	if (Robot::allianceColor == DriverStation::Alliance::kBlue){
 		AddParallel(new TurnToAngle(-90));
 	} else if (Robot::allianceColor == DriverStation::Alliance::kRed) {
 		AddParallel(new TurnToAngle(90));
 	}
-	AddSequential(new SpinUp(3, -SHOOTER_POWER));
+	AddSequential(new SpinUp(1.0, -SHOOTER_POWER));
+
 	AddParallel(new Kick(7, -.5));
 	AddSequential(new SpinUp(7, -SHOOTER_POWER));
+
 	if (Robot::allianceColor == DriverStation::Alliance::kBlue){
-		AddSequential(new DriveCartesian(0, .5, .3, true, 1.5));
 		AddSequential(new TurnToAngle(-60));
 		AddSequential(new DriveStraight(.7, .5));
 	} else if (Robot::allianceColor == DriverStation::Alliance::kRed) {
-			AddSequential(new DriveCartesian(0, .5, -.3, true, 1.5));
-			AddSequential(new TurnToAngle(60));
-			AddSequential(new DriveStraight(.7, .5));
+		AddSequential(new TurnToAngle(60));
+		AddSequential(new DriveStraight(.7, .5));
 	}
 
 
